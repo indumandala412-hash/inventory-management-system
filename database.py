f8d4af25f8e6df3037079b62c9953cb7b336836f
sample knowledge <sivasiva83059@gmail.com>
2026-06-24 11:50:33 +0530

    fix database url

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
  
+import os
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, URL
+from sqlalchemy import create_engine
 
-db_url="postgresql://postgres:your_password@localhost:5432/telusko"
-
-engine =create_engine(db_url)
-session = sessionmaker(autocommit=False,autoflush=True,bind=engine)
+db_url = os.getenv("postgresql://inventory_db_wcf7_user:bHu7UHXstmwDMOpvat6sTMpypNZHxsFV@dpg-d8tn98og4nts73d3rp00-a/inventory_db_wcf7")
+engine = create_engine(db_url)
+session = sessionmaker(autocommit=False, autoflush=True, bind=engine)
